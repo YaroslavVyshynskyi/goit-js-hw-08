c04ed21efe3ff525cfe8a54fb765e96752494bef
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-05-07 16:42:43 +0300

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createGalleryItemMarkup(elements) { 
+    return elements.map(({preview, description, original}) => { 
+        return `
+            <a
+            class="gallery__item"
+            href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title="${description}"
+            ></img></a>
+            `;
+        })
+    .join("");
+};
+
+let gallery = new SimpleLightbox('.gallery__item', { captionDelay: 250 });
