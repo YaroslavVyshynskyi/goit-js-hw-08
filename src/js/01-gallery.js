5650ffd583bd9cb6f3858090418c63051524d97e
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-05-08 20:00:29 +0300

    add vimeo player + throttle

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createGalleryItemMarkup(elements) { 
+    return elements.map(({preview, description, original}) => { 
+        return `
+            <a
+            class="gallery__item"
+            href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            title="${description}"
+            ></img></a>
+            `;
+        })
+    .join("");
+};
+
+let gallery = new SimpleLightbox('.gallery__item', { captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+var throttle = require('lodash.throttle');
+
+player.on('timeupdate',
+    throttle((data) => {
+    // function (data) {
+    videoplayerCurrentTime = data.seconds;
+    localStorage.setItem('seconds', videoplayerCurrentTime);
+    console.log(localStorage.getItem('seconds'));
+    }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('seconds')).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            
+            break;
+
+        default:
+            
+            break;
+    }
+});
